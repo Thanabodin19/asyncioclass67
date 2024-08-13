39ba54c725eabc3d225fc505f83118f3590b0833
Thanabodin19 <thanabodin.kea@gmail.com>
2024-08-14 01:55:03 +0700

    pokemon done

--- assignment07/pokemon/pokemon04.py
+++ assignment07/pokemon/pokemon04.py
@@ -11,6 +11,17 @@ async def main():
 
     # Iterate through all json files in the directory.
     for path in pathlist:
-        print(path)
+        # Read the contents of the json file.
+        async with aiofiles.open(f'./{path}', mode='r') as f:
+            contents = await f.read()
+        
+        # Load it into a dictionary and create list of moves.
+        pokemon = json.loads(contents)
+        name = pokemon['name']
+        moves = [move['move']['name'] for move in pokemon['moves']]
+
+        # Open a new file to write the list of moves into.
+        async with aiofiles.open(f'{pokemonmove_directory}/{name}_moves.txt', mode='w') as f:
+            await f.write('\n'.join(moves))
 
 asyncio.run(main())
